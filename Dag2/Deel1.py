0cec7685cba9513d53f013ab6ea0fcb1045e07a3
hfsdo <thijsdl@gmail.com>
2024-12-02 19:58:31 +0100

    Dag 2 Deel 1 (bugged)

---
+++ Dag2/Deel1.py
@@ -0,0 +1,33 @@
+reportlist = []
+
+with open('Dag2\\input.txt', 'r') as file:
+    for line in file:
+        reportlist.append(line.replace("\n", ""))
+
+safe = 0
+for report in reportlist:
+    bsafe = True
+    
+    #print(f"report: {report}")
+    levels = report.split(" ")
+    increase = int(levels[1]) - int(levels[0])
+    #print(f"increase: {increase}")
+    for i in range(1, len(levels)):
+        if int(levels[i]) <= int(levels[i-1]) and increase>0:
+            bsafe = False #decreasing
+            #print("decrease")
+        if int(levels[i]) >= int(levels[i-1]) and increase<0:
+            bsafe = False #increasing
+            #print("increase")
+        if int(levels[i]) > int(levels[i-1])+3 and increase>0:
+            bsafe = False #increase too much
+            #print("fast increase")
+        if int(levels[i]) < int(levels[i-1])-3 and increase<0:
+            bsafe = False #decrease too much
+            #print("fast decrease")
+    if bsafe:
+        safe+=1
+
+print(f"safe reports: {safe}")
+            
+    
