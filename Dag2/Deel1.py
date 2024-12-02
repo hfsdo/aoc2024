7d8b3871484c8bc986b0fb5941e926026c8da4df
hfsdo <thijsdl@gmail.com>
2024-12-02 21:11:58 +0100

    Dag 2 Deel 1

--- Dag2/Deel1.py
+++ Dag2/Deel1.py
@@ -7,24 +7,28 @@ with open('Dag2\\input.txt', 'r') as file:
 safe = 0
 for report in reportlist:
     bsafe = True
-    
-    #print(f"report: {report}")
+    print("---------")
+    print(f"report: {report}")
     levels = report.split(" ")
     increase = int(levels[1]) - int(levels[0])
-    #print(f"increase: {increase}")
+    print(f"increase: {increase}")
     for i in range(1, len(levels)):
-        if int(levels[i]) <= int(levels[i-1]) and increase>0:
-            bsafe = False #decreasing
-            #print("decrease")
-        if int(levels[i]) >= int(levels[i-1]) and increase<0:
-            bsafe = False #increasing
-            #print("increase")
-        if int(levels[i]) > int(levels[i-1])+3 and increase>0:
-            bsafe = False #increase too much
-            #print("fast increase")
-        if int(levels[i]) < int(levels[i-1])-3 and increase<0:
-            bsafe = False #decrease too much
-            #print("fast decrease")
+        if bsafe:
+            if levels[i] == levels[i-1]:
+                bsafe = False
+                print("same")
+            if int(levels[i]) <= int(levels[i-1]) and increase>0:
+                bsafe = False #decreasing
+                print("decrease")
+            if int(levels[i]) >= int(levels[i-1]) and increase<0:
+                bsafe = False #increasing
+                print("increase")
+            if int(levels[i]) > int(levels[i-1])+3 and increase>0:
+                bsafe = False #increase too much
+                print("fast increase")
+            if int(levels[i]) < int(levels[i-1])-3 and increase<0:
+                bsafe = False #decrease too much
+                print("fast decrease")
     if bsafe:
         safe+=1
 
